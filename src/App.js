d6ddd9a7cacc30d816e6e3c779fc237d3ae5ee4d
tonycito <apenaalcedo@gmail.com>
2021-01-15 16:36:23 -0500

    cambio nuevo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import './App.css';
-import Navbar from './components/Navbar';
-
+import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <>
       <Navbar />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-    return (
-        <nav>
-            <Link to='/'>HOLAAAAAAAAAA</Link>
-        </nav>
-    )
+  return (
+    <nav>
+      <Link to="/" className=" text-6xl text-red-500">
+        HOLAAAAAAAAAA
+      </Link>
+      <div className="px-4 cursor-pointer md:hidden">
+        <svg
+          className="w-6 h-6"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M4 6h16M4 12h16M4 18h16"
+          />
+        </svg>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
