d7382cbcf57d4f3914c2bef42b690a9088cfbcc3
tonycito <apenaalcedo@gmail.com>
2021-01-15 11:38:07 -0500

    commit actualizacion

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <Link to='/'>HOLAAAAAAAAAA</Link>
+        </nav>
+    )
+}
+
+export default Navbar
